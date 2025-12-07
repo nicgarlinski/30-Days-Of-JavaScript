1cc55650d0d6dc23b3ef6a65f10b0a319693d877
nicgarlinski <nicgarlinski@gmail.com>
2025-12-07 23:08:11 +0100

    30 days of js day 1
    I am already familiar with JS however I have some gaps in my knowledge and i was recommended this course. I prefer to start with day 1 to remember the basics and move on from there

---
+++ 01_Day_Introduction/01_day_starter/datatypes_day_one.js
@@ -0,0 +1,26 @@
+let nullone
+let nulltwo
+let nullthree
+let nullfour
+let fullone = 1
+let fulltwo = 'two'
+let fullthree = true
+let fullfour = 'i already know this'
+let FirstNameUser = 'Nicolas'
+let LastNameUser = 'Garlinski'
+let Age = '23'
+let Married = false
+let Name = 'Nicolas', Last = 'Garlinski', Userage = '23', Marriage = 'false'
+let Myage = '25'
+let Yourage = '30'
+console.log(typeof nullone) 
+console.log(typeof nulltwo) 
+console.log(typeof nullthree) 
+console.log(typeof nullfour) 
+console.log(typeof fullone)
+console.log(typeof fulltwo)
+console.log(typeof fullthree)
+console.log(typeof fullfour)
+console.log(Myage)
+console.log(Yourage)
+
--- 01_Day_Introduction/01_day_starter/helloworld.js
+++ 01_Day_Introduction/01_day_starter/helloworld.js
@@ -1,1 +1,7 @@
 console.log('Hello, World!')
+//comments can make code readable
+//Welcome to 30DaysOfJavaScript
+/*
+comments can make code readable,
+easy to reuse and informative
+*/
